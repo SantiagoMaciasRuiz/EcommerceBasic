ce52e5d6aa14bcc247d4d400cda521df6239e6db
Santiago Macias <smaciasr@unal.edu.co>
2024-11-05 13:08:36 -0500

    Completed-prueba de GET y POST

--- product-service/src/main/java/com/ecommerce/product_service/Producto/Product.java
+++ product-service/src/main/java/com/ecommerce/product_service/Producto/Product.java
@@ -1,7 +1,8 @@
 package com.ecommerce.product_service.Producto;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+@Entity
 public class Product {
         @Id
         private String id;
