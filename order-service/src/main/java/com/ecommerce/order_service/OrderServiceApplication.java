19bec2febade504c1f4a3eb938267946f6a659a0
Santiago Macias <smaciasr@unal.edu.co>
2024-11-05 09:08:39 -0500

    creacion del orderService -> incomplete

--- order-service/src/main/java/com/ecommerce/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/ecommerce/order_service/OrderServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
